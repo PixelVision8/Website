871b6f07996d0631344daabc19da0b0c3303e0c0
Goomboo <87389048+Goomboo@users.noreply.github.com>
2021-07-14 14:55:39 +0100

    Fix 404 page

--- site/src/pages/404.js
+++ site/src/pages/404.js
@@ -3,8 +3,13 @@ import Layout from '../components/layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div class="flex mb-20 mx-auto max-w-3xl bg-palette-5 border-t-4 border-palette-6 rounded-b text-palette-15 px-4 py-3 shadow-md" role="alert">
+      <Image src={require('../images/icons/file-drive-icon.svg')} alt="File Drive Icon" width={92} height={92} />
+      <div className="ml-5">
+        <p class="h4 text-palette-15">Not Found</p>
+        <p class="text-m">You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </div>
+    </div>
   </Layout>
 )
 
